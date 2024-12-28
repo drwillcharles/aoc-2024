2a4ec560c64cd2998ab43878203c54bbce8e35f2
Will Charles <williamgeorgecharles@gmail.com>
2024-12-28 17:17:56 +0000

    Fixing mutuble references

--- day5/src/bin/part2.rs
+++ day5/src/bin/part2.rs
@@ -2,7 +2,7 @@ use std::fs;
 // use std::collections::HashMap;
 use multimap::MultiMap;
 
-fn create_map(binding :&String) -> MultiMap<i32,i32> {
+fn create_map(binding: &String) -> MultiMap<i32, i32> {
     let mut after_before = MultiMap::new();
 
     for line in binding.lines() {
@@ -20,7 +20,7 @@ fn create_map(binding :&String) -> MultiMap<i32,i32> {
         after_before.insert(after, before);
     }
 
-    return after_before
+    return after_before;
 }
 
 fn create_updates(binding: &String) -> Vec<Vec<i32>> {
@@ -30,46 +30,57 @@ fn create_updates(binding: &String) -> Vec<Vec<i32>> {
         updates.push(line.split(',').map(|n| n.parse().unwrap()).collect())
     }
 
-    return updates
+    return updates;
 }
 
-fn check_order(after_before: &MultiMap<i32,i32>, updates_after: &[i32], val: &i32) -> bool {
+fn check_order(
+    after_before: &MultiMap<i32, i32>,
+    update: &mut Vec<i32>,
+    i: &usize,
+    val: &i32,
+) -> bool {
+    let mut valid: bool = true;
     let check = after_before.get_vec(val);
-        // Using pattern matching (recommended)
-        match check {
-            Some(number) => {
-                // println!("The number is: {}", number);
-                // Perform your action with the number here
-                for before in number {
-                    if updates_after.contains(before) {
-                        println!("Invalid {:?} in {:?}", number, updates_after);
-                        return false;
-                    }
+    // Using pattern matching (recommended)
+    match check {
+        Some(number) => {
+            // println!("The number is: {}", number);
+            // Perform your action with the number here
+            for before in number {
+                if update[*i..].contains(before) {
+                    //Change to while loop
+                    println!("Invalid {:?} in {:?}", number, update);
+                    // Fix value
+                    update.retain(|&x| x != *val);
+                    update.insert(i - 1, *before);
+
+                    valid = false;
                 }
             }
-            None => {
-                // println!("The number is not present.");
-                return true
-            }
         }
-        return true
+        None => {
+            // println!("The number is not present.");
+            // return true
+        }
+    }
+    return valid;
 }
-fn check_updates(after_before: &MultiMap<i32,i32>, updates: &Vec<Vec<i32>>) -> i32 {
+fn check_updates(after_before: &MultiMap<i32, i32>, updates: &mut Vec<Vec<i32>>) -> i32 {
     // Now we check the rules
-    let mut total:i32 = 0;
+    let mut total: i32 = 0;
 
-    for update in updates {
-        let mut valid:bool;
-        for (i, val) in update.iter().enumerate() {
+    for mut update in updates.iter_mut() {
+        let mut valid: bool;
+        for (i, val) in update.iter_mut().enumerate() {
             // Check if page has appeared
-            valid = check_order(&after_before, &update[i..], &val);
+            valid = check_order(&after_before, &mut update, &i, &val);
 
             // Don't think we need this for part 2
             if !valid {
-                break
+                break;
             }
 
-            if i == update.len() - 1  && valid{
+            if i == update.len() - 1 && valid {
                 let middle = update[update.len() / 2];
                 println!("Middle number is {:?}", middle);
                 total += middle;
@@ -87,7 +98,7 @@ fn main() {
 
     // if parts.len() != 2 {
     //     println!("Error: Expected exactly two parts separated by '|'");
-    //     return Ok(()); 
+    //     return Ok(());
     // }
 
     let part1 = parts[0].to_string();
@@ -106,12 +117,9 @@ fn main() {
     let after_before = create_map(&part1);
 
     // Now create lists of updates
-    let updates = create_updates(&part2);
+    let mut updates = create_updates(&part2);
 
-    let total = check_updates(&after_before, &updates);
+    let total = check_updates(&after_before, &mut updates);
 
     println!("{:?}", total);
-
-
 }
-
